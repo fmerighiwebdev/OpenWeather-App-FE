45cade7c1186fcad4fd2efba0965a9085f7fe06b
francescomerighi1202 <francescomerighi61@gmail.com>
2024-03-26 10:13:46 +0100

    Inizializzazione progetto (migrazione)

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Header from './components/Header';
+
+import './styles/App.css';
+
+function App() {
+
+  const [city, setCity] = React.useState(sessionStorage.getItem("city")) || "";
+
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={
+          <>
+            <Header />
+            <Home setCity={setCity} city={city} />
+          </>
+        } />
+        <Route exact path='/weather-data' element={
+          <>
+            <Dashboard city={city} />
+            <Footer />
+          </>
+        } />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='*' element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
