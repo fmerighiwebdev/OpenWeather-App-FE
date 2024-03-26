694a65f37cabe248c18c6c12bc83c01ad29165a5
francescomerighi1202 <francescomerighi61@gmail.com>
2024-03-26 11:55:27 +0100

    Aggiunta logica registrazione utente

--- src/client-utils.js
+++ src/client-utils.js
@@ -1,21 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 async function getWeather(city, setWeatherData, setLoading) {
+  try {
+    const coordinates = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
+    );
+    const { lat, lon } = coordinates.data[0];
 
-    try {
-        const coordinates = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
-        const { lat, lon } = coordinates.data[0];
-
-        const weather = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`);
-        setWeatherData(weather.data);
-    } catch(err) {
-        console.log(err);
-    } finally {
-        setLoading(false);
-    }
-    
+    const weather = await axios.get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+    );
+    setWeatherData(weather.data);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    setLoading(false);
+  }
 }
 
-export { getWeather };
+async function signUp(name, username, email, password) {
+  try {
+    const response = await axios.post("http://localhost:5000/api/signup", {
+      name,
+      username,
+      email,
+      password
+    });
+    console.log(response.data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export { getWeather, signUp };
--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,30 +1,107 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "../styles/Auth.css";
 
+import { signUp } from "../client-utils";
+
 function AuthForm({ type }) {
+  const [user, setUser] = React.useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  function handleChanges(e) {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (type === "signup") {
+      signUp(user.name, user.username, user.email, user.password);
+      navigate("/login");
+    }
+
+    setUser({
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    });
+  }
+
   return (
-    <form>
-      <h1>{type === "login" ? "Accedi" : "Registrati"}</h1>
+    <form onSubmit={handleSubmit}>
+      <h2>{type === "login" ? "Accedi" : "Registrati"}</h2>
       {type === "signup" && (
-        <>
-          <label htmlFor="name">Nome</label>
-          <input type="text" id="name" name="name" required />
-        </>
+        <div className="form-block">
+          <label htmlFor="name">Nome*</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={user.name}
+            required
+            onChange={handleChanges}
+          />
+        </div>
       )}
       {type === "signup" && (
-        <>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" required />
-        </>
+        <div className="form-block">
+          <label htmlFor="username">Username*</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={user.username}
+            required
+            onChange={handleChanges}
+          />
+        </div>
       )}
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" required />
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" name="password" required />
+      <div className="form-block">
+        <label htmlFor="email">Email*</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={user.email}
+          required
+          onChange={handleChanges}
+        />
+      </div>
+      <div className="form-block">
+        <label htmlFor="password">Password*</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={user.password}
+          required
+          onChange={handleChanges}
+        />
+      </div>
       <button type="submit">
         {type === "login" ? "Accedi" : "Registrati"}
       </button>
+      {type === "login" ? (
+        <p>
+          Non hai un account? <Link to="/signup">Registrati</Link>
+        </p>
+      ) : (
+        <p>
+          Hai già un account? <Link to="/login">Accedi</Link>
+        </p>
+      )}
     </form>
   );
 }
